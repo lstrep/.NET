b238c172ac4523287c17ce9e20973a69d705d0dc
lstrep <95477806+lstrep@users.noreply.github.com>
2022-05-25 20:45:12 +0200

    Delete Linq directory

--- Linq/LinqTutorial/LinqTutorials/Models/Dept.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LinqTutorials.Models
-{
-    public class Dept
-    {
-        public int Deptno { get; set; }
-        public string Dname { get; set; }
-        public string Loc { get; set; }
-    }
-}
